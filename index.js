207e83ccf83b9a18e19cb51079b8b7ca6c6f1e6a
unknown <fahrisenpaisenpai@gmail.com>
2025-07-31 22:15:19 +0700

    feat(users): update users

--- index.js
+++ index.js
@@ -40,3 +40,12 @@ const getTodos = async () => {
 }
 
 getTodos()
+
+const getUsers = async () => {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users')
+       const resultObj =  await result.json ()
+       console.log (resultObj[0].username)
+       console.log (resultObj[0].email)
+}
+
+getUsers()
